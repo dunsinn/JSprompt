3d06438dba982e05bd97be8c368cca4c22865b30
Lawal Oluwatosin <105178982+dunsinn@users.noreply.github.com>
2022-07-17 04:14:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const myName = prompt("What is your name?");
+const myHeight = prompt("How tall are you?(in feet and inches)");
+const myCountry = prompt("What country are you from?");
+
+alert("My name is " + myName + ".\nI am "+ myHeight + "tall.\nI am from " + myCountry + ".");
